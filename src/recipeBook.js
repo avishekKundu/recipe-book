41ab76b36e72f120e17a13a756cb4874deac01d4
Avishek Kundu <119733078+avishekKundu@users.noreply.github.com>
2023-10-02 00:03:31 +0530

    Recipe Book ready

--- src/recipeBook.js
+++ src/recipeBook.js
@@ -28,45 +28,55 @@ const RecipeBook = () => {
                 <div className="container-fluid p-2 titleHead">
                     <center>Recipe Book</center>
                 </div>
-                <div className="container-fluid d-flex justify-content-between inputContainer">
-                    <div>
-                        <input
-                            className="border border-4 border-secondary recipeNameContainer"
-                            type="text"
-                            placeholder="Enter recipe name"
-                            value={recipes.name} onChange={(e) => setRecipes({ ...recipes, name: e.target.value })} />
-                    </div>
-                    <div>
-                        <textarea
-                            className="border border-4 border-secondary ingredientContainer"
-                            placeholder="Enter recipe ingredients"
-                            value={recipes.ingredients} onChange={(e) => setRecipes({ ...recipes, ingredients: e.target.value })} />
-                    </div>
-                    <div>
-                        <input
-                            className="border border-4 border-secondary categoryContainer"
-                            type="text"
-                            placeholder="Enter category"
-                            value={recipes.category} onChange={(e) => setRecipes({ ...recipes, category: e.target.value })} />
-                        <button onClick={addRecipe} className="btn btn-primary border border-4 btnAdd">Add Recipe</button>
+                <div className="main_page">
+                    <div className="recipe_main">
+                        <div className="container-fluid d-flex justify-content-between inputContainer">
+                            <div >
+                                <input
+                                    className="border border-4 border-secondary recipeNameContainer"
+                                    type="text"
+                                    placeholder="Enter recipe name"
+                                    value={recipes.name} onChange={(e) => setRecipes({ ...recipes, name: e.target.value })} />
+                            </div>
+
+                            <div>
+                                <input
+                                    className="border border-4 border-secondary categoryContainer"
+                                    type="text"
+                                    placeholder="Enter category"
+                                    value={recipes.category} onChange={(e) => setRecipes({ ...recipes, category: e.target.value })} />
+
+                            </div>
+                            <div>
+                                <textarea
+                                    className="border border-4 border-secondary ingredientContainer"
+                                    placeholder="Enter recipe ingredients"
+                                    value={recipes.ingredients} onChange={(e) => setRecipes({ ...recipes, ingredients: e.target.value })} />
+                            </div>
+                            <div className="button_base">
+                                <button onClick={addRecipe} className="add_btn border border-4 border-secondary">Add Recipe</button>
+                            </div>
+                        </div>
+                        <div className="container-fluid recipeListContainer">
+                            <ul className="list-group recipe-list">
+                                {recipeList.map((recipe, index) => (
+                                    <li key={index} className="recipeLists">
+                                        <div className="card p-2 listContainer">
+                                            <div className="card-body cardBody">
+                                                <p className="card-text recipeCategory"><span>Category:</span> {recipe.category}</p>
+                                                <p className="card-text recipeName"><span>Recipe:</span> {recipe.name}</p>
+                                                <p className="card-text recipeIngredient"><span>Ingredients:</span>  {recipe.ingredients}</p>
+                                                <button onClick={() => deleteRecipe(index)} className="btn btn-danger border border-3 btnRemove">Delete</button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="container-fluid recipeListContainer">
-                    <ul className="list-group recipe-list">
-                        {recipeList.map((recipe, index) => (
-                            <li key={index} className="recipeLists">
-                                <div className="card p-2 listContainer">
-                                    <div className="card-body cardBody">
-                                        <p className="card-text recipeCategory"><span>Category:</span> {recipe.category}</p>
-                                        <p className="card-text recipeName"><span>Recipe:</span> {recipe.name}</p>
-                                        <p className="card-text recipeIngredient"><span>Ingredients:</span>  {recipe.ingredients}</p>
-                                        <button onClick={() => deleteRecipe(index)} className="btn btn-danger border border-3 btnRemove">Delete</button>
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+
+
             </div>
         </>
     )
